6da860332b7e862223c6ef586e98d1a523b76d70
Carlos Lopez <carlosboblopez@gmail.com>
2015-06-15 03:28:06 -0700

    Update ConfigParser.fsx
    Discarded custom config format to JSON based one. Started expanding config parameters.

--- src/ConfigParser.fsx
+++ src/ConfigParser.fsx
@@ -1,40 +1,51 @@
 module RFTimeline.ConfigParser
 
+#r "../packages/FSharp.Data/FSharp.Data.dll"
+
 open System
 open System.IO
-open System.Text.RegularExpressions
+open FSharp.Data
 
 //TODO: Allow custom video id partition size
-type AppConfig = {APIKey : string; PlaylistIDs : string list} 
+type AppConfig = 
+    { APIKey : string
+      PlaylistIDs : string list
+      Filtered : string list
+      Ungrouped : string list } 
 
-let selectfirstmatchgroup regex line =
-    Regex.Match(line,regex) // Theope following throws an exception if there are no match groups defined.
-    |> fun result -> if result.Success then Some((result.Groups.Item 1).Value) else None
 
-let parseapikey line = selectfirstmatchgroup "^apikey: {0,1}(\S+)" line // Allow apikey:key or apikey: key ignore everything after
-let parseplaylistid line = selectfirstmatchgroup "^(\S+)" line // Grab first value before a space.
+[<Literal>]
+let configformat = 
+    """
+{
+  "apikey": "youtube api key",
+  "playlists": [
+    {"Channel":"channel name", "PlaylistID":"channel id"}, 
+    {"Channel":"channel name", "PlaylistID":"channel id"}
+  ],
+  "ungrouped": [
+    "video id 1",
+    "video id 2"
+  ],
+  "filtered": [
+    "ignored video id 1",
+    "ignored video id 2"
+  ]
+}
+    """
 
-//TODO: Optimize    
-let parseconfig lines =
-    let apikey = Seq.head lines |> parseapikey
-    let playlistids = Seq.skip 2 lines |> Seq.map(parseplaylistid)
-    match apikey with
-    | None -> None
-    | Some apikey ->
-        let addplaylistids ids = {APIKey = apikey; PlaylistIDs = ids} 
-        match Seq.forall(function Some _ -> true | None -> false) playlistids with
-        | false -> None
-        | true -> Some (addplaylistids (Seq.choose id playlistids |> Seq.toList))
-        
-let openconfig path =
+
+type ConfigParser = JsonProvider<configformat>
+
+let openconfig (path : string) =
     try
-        let filelines = File.ReadAllLines(path) //File.ReadLines apparently closing and throwing exception with message that stream is closed
-        match parseconfig filelines with //should dispose properly
-        | Some config -> Some config
-        | None -> 
-            do printfn "Error parsing config file at %s:" path
-            None
+        let config = ConfigParser.Load(path)
+        let apikey = config.Apikey
+        let playlists = List.ofArray (config.Playlists |> Array.map(fun c -> c.PlaylistId))
+        let filtered = List.ofArray config.Filtered
+        let ungrouped = List.ofArray config.Ungrouped
+        Some { APIKey = apikey; PlaylistIDs = playlists; Filtered = filtered; Ungrouped = ungrouped }
     with
     | ex ->
         do printfn "Error opening config file at %s: %s" path ex.Message
-        None    
+        None
